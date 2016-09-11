dfe7e4d88e12c48dab4164ac2ecf9e96feea401b
chendi <cdsama@gmail.com>
2016-09-11 17:31:16 +0800

    change test usage

--- test/Main.cpp
+++ test/Main.cpp
@@ -83,24 +83,23 @@ void TestNamespace(LuaState& ls)
         .EndEnum()
         ;
 
-    auto ErrorHandle = [](const std::string& error)
-    {
-        REDLOG(error);
-    };
-
-    ls.DoFile("main.lua", ErrorHandle);
+    ls.DoFile("main.lua");
 
     ls.DoString("local a = test.A() a:Print()");
     ls.DoString("b = test.B.GetInstance() b:Print()");
     ls.DoString("b.name = 'name' b.id = 12345 print('b.name = ',b.name,'b.id = ',b.id)");
     ls.DoString("b:lambdatest1() print('b.name = ',b.name)");
     assert(ls.GetGlobal("b").Cast<B*>() == B::GetInstance());
-    ls.DoString("b:RiseID() print('b.readonlyid = ',b.readonlyid) b.readonlyid = 456789", ErrorHandle);
+    ls.DoString("b:RiseID() print('b.readonlyid = ',b.readonlyid) b.readonlyid = 456789");
     assert(ls.GetGlobal("test")["B"]["lambdatest2"]().Cast<std::string>() == "B.lambdatest2()");
     assert(ls.GetGlobal("test")["lambdatest3"]().Cast<B*>() == B::GetInstance());
 
-    ls.DoString("b.TestSTDFunction = function(a, b) return tostring(a) .. '&' .. tostring(b) end", ErrorHandle);
+    ls.DoString("b.TestSTDFunction = function(a, b) return tostring(a) .. '&' .. tostring(b) end");
     assert(B::GetInstance()->TestSTDFunction(7, 8) == "7&8");
+    ls.DoString("b.TestSTDFunction('haha', 'haha')");
+
+    ls.DoString("b.TestSTDFunction = function(a, b) return 0 end");
+    assert(B::GetInstance()->TestSTDFunction(7, 8) == "");
 
     ls.DoString("tb = { a = 1, b = 2, c = { d = 3, e = 'f', g = { h = 4, i = { 5 } } } }");
     auto tb = ls.GetGlobal("tb");
@@ -141,6 +140,10 @@ void TestNamespace(LuaState& ls)
     assert(ls.GetGlobal("TestEnum")["Value1"].Cast<TestEnum>() == TestEnum::Value1);
     assert(ls.GetGlobal("TestEnum")["Value2"].Cast<TestEnum>() == TestEnum::Value2);
 
+    ls.DoString("function func3() error('error') end");
+    ls.GetGlobal("func3")()()();
+
+
     // Should clear registed std::function before luastate closed. 
     B::GetInstance()->TestSTDFunction = nullptr;
 }
@@ -164,36 +167,80 @@ void TestStack(LuaState& ls)
     char const* cc = "";
     std::string ss = "";
 
-    Push(L, i);  lua_pop(L, 1);
-    Push(L, l);  lua_pop(L, 1);
-    Push(L, b);  lua_pop(L, 1);
-    Push(L, s);  lua_pop(L, 1);
-    Push(L, f);  lua_pop(L, 1);
-    Push(L, d);  lua_pop(L, 1);
-    Push(L, ui); lua_pop(L, 1);
-    Push(L, uc); lua_pop(L, 1);
-    Push(L, us); lua_pop(L, 1);
-    Push(L, ul); lua_pop(L, 1);
-    Push(L, c);  lua_pop(L, 1);
-    Push(L, cc); lua_pop(L, 1);
-    Push(L, ss); lua_pop(L, 1);
+#define TEST_TYPE_CHECK_DEFAULT(__TYPE__) if (!Stack<__TYPE__>::CheckType(L, -1)) \
+    { \
+        REDLOG("Type Check failed :"<< typeid(__TYPE__).name()); \
+        __TYPE__ t = Stack<__TYPE__>::DefaultValue(L, -1); \
+    }
+
+    Push(L, i);
+    TEST_TYPE_CHECK_DEFAULT(int);
+    lua_pop(L, 1);
+    Push(L, l);  
+    TEST_TYPE_CHECK_DEFAULT(long);
+    lua_pop(L, 1);
+    Push(L, b);  
+    TEST_TYPE_CHECK_DEFAULT(bool);
+    lua_pop(L, 1);
+    Push(L, s);  
+    TEST_TYPE_CHECK_DEFAULT(short);
+    lua_pop(L, 1);
+    Push(L, f);  
+    TEST_TYPE_CHECK_DEFAULT(float);
+    lua_pop(L, 1);
+    Push(L, d);  
+    TEST_TYPE_CHECK_DEFAULT(double);
+    lua_pop(L, 1);
+    Push(L, ui); 
+    TEST_TYPE_CHECK_DEFAULT(unsigned int);
+    lua_pop(L, 1);
+    Push(L, uc); 
+    TEST_TYPE_CHECK_DEFAULT(unsigned char);
+    lua_pop(L, 1);
+    Push(L, us); 
+    TEST_TYPE_CHECK_DEFAULT(unsigned short);
+    lua_pop(L, 1);
+    Push(L, ul); 
+    TEST_TYPE_CHECK_DEFAULT(unsigned long);
+    lua_pop(L, 1);
+    Push(L, c);  
+    TEST_TYPE_CHECK_DEFAULT(char);
+    lua_pop(L, 1);
+    Push(L, cc); 
+    TEST_TYPE_CHECK_DEFAULT(const char*);
+    lua_pop(L, 1);
+    Push(L, ss); 
+    TEST_TYPE_CHECK_DEFAULT(std::string);
+    lua_pop(L, 1);
 
     A a;
     A const ac(a);
 
     Push(L, &a);
+    TEST_TYPE_CHECK_DEFAULT(A*);
     Push(L, &ac);
+    TEST_TYPE_CHECK_DEFAULT(A const*);
 
     Push <A&>(L, a);
+    TEST_TYPE_CHECK_DEFAULT(A&);
     Push <A const&>(L, a);
+    TEST_TYPE_CHECK_DEFAULT(A const &);
 
     RefCountedObjectPtr <C> pc;
     RefCountedObjectPtr <C const> pcc;
 
-    Push(L, pc);   lua_pop(L, 1);
-    Push(L, pcc);  lua_pop(L, 1);
-    Push(L, a);    lua_pop(L, 1);
-    Push(L, ac);   lua_pop(L, 1);
+    Push(L, pc);   
+    TEST_TYPE_CHECK_DEFAULT(RefCountedObjectPtr <C>);
+    lua_pop(L, 1);
+    Push(L, pcc);  
+    TEST_TYPE_CHECK_DEFAULT(RefCountedObjectPtr <C const>);
+    lua_pop(L, 1);
+    Push(L, a);    
+    TEST_TYPE_CHECK_DEFAULT(A);
+    lua_pop(L, 1);
+    Push(L, ac);   
+    TEST_TYPE_CHECK_DEFAULT(A const);
+    lua_pop(L, 1);
 
     Stack<A*>::Push(L, &a); lua_pop(L, 1);
     Stack<A* const>::Push(L, &a); lua_pop(L, 1);
@@ -209,8 +256,32 @@ void TestStack(LuaState& ls)
     Stack<std::function<void()>>::Push(L, pushfunc);
     Stack<std::function<void()>>::Get(L, -1)();
     Stack<std::function<int(int)>>::Push(L, pushfunc2);
+    TEST_TYPE_CHECK_DEFAULT(std::function<void()>);
     Stack<std::function<int(int)>>::Get(L, -1)(1);
     lua_pop(L, 2);
+
+    lua_newtable(L);
+    TEST_TYPE_CHECK_DEFAULT(int);
+    TEST_TYPE_CHECK_DEFAULT(long);
+    TEST_TYPE_CHECK_DEFAULT(bool);
+    TEST_TYPE_CHECK_DEFAULT(short);
+    TEST_TYPE_CHECK_DEFAULT(float);
+    TEST_TYPE_CHECK_DEFAULT(double);
+    TEST_TYPE_CHECK_DEFAULT(unsigned int);
+    TEST_TYPE_CHECK_DEFAULT(unsigned char);
+    TEST_TYPE_CHECK_DEFAULT(unsigned short);
+    TEST_TYPE_CHECK_DEFAULT(unsigned long);
+    TEST_TYPE_CHECK_DEFAULT(char);
+    TEST_TYPE_CHECK_DEFAULT(const char*);
+    TEST_TYPE_CHECK_DEFAULT(std::string);
+    TEST_TYPE_CHECK_DEFAULT(A*);
+    TEST_TYPE_CHECK_DEFAULT(A const*);
+    TEST_TYPE_CHECK_DEFAULT(A&);
+    TEST_TYPE_CHECK_DEFAULT(A const &);
+    TEST_TYPE_CHECK_DEFAULT(RefCountedObjectPtr <C>);
+    TEST_TYPE_CHECK_DEFAULT(RefCountedObjectPtr <C const>);
+    TEST_TYPE_CHECK_DEFAULT(A);
+    TEST_TYPE_CHECK_DEFAULT(A const);
 
     lua_settop(L, idx);
 }
